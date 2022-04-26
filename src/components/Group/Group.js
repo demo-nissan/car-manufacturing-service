8434bb78947402ea8295e79edfa828d2494ae466
mijoyjos <mijoyjos@gmail.com>
2022-04-26 10:32:27 +0530

    Edit popup ui changes

--- src/components/Group/Group.js
+++ src/components/Group/Group.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Button from '@mui/material/Button';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -8,28 +9,36 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import './Group.module.css';
+import EditIcon from '@mui/icons-material/Edit';
+
+import Modal from '@mui/material/Modal';
+import Backdrop from '@mui/material/Backdrop';
+import CreateUpdate from '../createUpdate';
 
 const mockData = [{
     GroupCode: "1",
-    GroupName : "Wheel",
-    active:true
-},{
+    GroupName: "Wheel",
+    active: true
+}, {
     GroupCode: "2",
-    GroupName : "Interior",
-    active:false
+    GroupName: "Interior",
+    active: false
 },
 {
     GroupCode: "3",
-    GroupName : "Exterior",
-    active:true
+    GroupName: "Exterior",
+    active: true
 }
 ]
-function Group(props) {
+function Group({ menuTabValue }) {
     const [groupData, setGroupData] = React.useState(mockData);
-    const handleClick = (newValue,i) => {
-        console.log(newValue,i);
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const handleClick = (newValue, i) => {
+        console.log(newValue, i);
         const list = [...groupData];
-        list[i]['active']=newValue?false:true;
+        list[i]['active'] = newValue ? false : true;
         setGroupData(list);
     };
     return (
@@ -44,22 +53,37 @@ function Group(props) {
                             <TableCell >Status</TableCell>
                         </TableRow>
                     </TableHead>
+                    <Modal
+                        aria-labelledby="transition-modal-title"
+                        aria-describedby="transition-modal-description"
+                        open={open}
+                        onClose={handleClose}
+                        closeAfterTransition
+                        BackdropComponent={Backdrop}
+                        BackdropProps={{
+                            timeout: 500,
+                        }}
+                    >
+                        <CreateUpdate createOrUpdateStatus={'Update'} handleClose={handleClose} headerMenuValue={menuTabValue} />
+                    </Modal>
                     <TableBody>
                         {
-                        groupData.map((data,i)=>{
-                            return(<TableRow
-                            key="1"
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell > {data.GroupCode}</TableCell>
-                            <TableCell >{data.GroupName}</TableCell>
-                            <TableCell ><Button variant="outlined" color="info">Edit</Button></TableCell>
-                            <TableCell ><Button variant="contained" onClick={() => { handleClick(data.active,i) }} 
-                            color={data.active ? 'success' : 'error'}>{data.active ? 'Activate' : 'Deactivate'}</Button>
-                            </TableCell>
-                        </TableRow>)
-                        }
-                        )
+                            groupData.map((data, i) => {
+                                return (<TableRow
+                                    key="1"
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell > {data.GroupCode}</TableCell>
+                                    <TableCell >{data.GroupName}</TableCell>
+                                    <TableCell ><Button variant="contained" color="info" startIcon={<EditIcon />}
+                                        onClick={handleOpen}>Edit</Button></TableCell>
+                                    <TableCell ><Button variant="contained" onClick={() => { handleClick(data.active, i) }}
+                                        color={data.active ? 'success' : 'error'}>{data.active ? 'Activate' : 'Deactivate'}
+                                    </Button>
+                                    </TableCell>
+                                </TableRow>)
+                            }
+                            )
                         }
                     </TableBody>
                 </Table>
