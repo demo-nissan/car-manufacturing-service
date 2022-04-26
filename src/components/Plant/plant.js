f4f9b0dc0ef1aa12e0ca0cffdf6b2264b3deebd9
demo-nissan <jojojoseph1096@gmail.com>
2022-04-26 23:08:32 +0530

    Updated pagination and update Data

--- src/components/Plant/plant.js
+++ src/components/Plant/plant.js
@@ -9,6 +9,9 @@ import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import Backdrop from '@mui/material/Backdrop';
+import EditIcon from '@mui/icons-material/Edit';
+import TablePagination from '@mui/material/TablePagination';
+
 import CreateUpdate from '../createUpdate';
 
 function Plant({menuTabValue}) {
@@ -18,6 +21,7 @@ function Plant({menuTabValue}) {
             plantCode: "2c1",
             plantName : "Nissan",
             country:"India",
+            active: true,
                 Group: [{
                     GroupCode: "2c1",
                     GroupName : "Nissan",
@@ -39,6 +43,7 @@ function Plant({menuTabValue}) {
                 plantCode: "2c2",
                 plantName : "Nissan",
                 country:"India",
+                active: false,
                     Group: [{
                         GroupCode: "2c1",
                         GroupName : "Nissan",
@@ -56,27 +61,137 @@ function Plant({menuTabValue}) {
                         },]
             
                 },
+                {
+                    plantCode: "2c3",
+                    plantName : "Nissan",
+                    country:"India",
+                    active: false,
+                        Group: [{
+                            GroupCode: "2c1",
+                            GroupName : "Nissan",
+                                Zone: [{
+                                ZoneCode: "2c1",
+                                ZoneName : "Nissan",
+                                }]
+                            },{
+                            GroupCode: "2c1",
+                            GroupName : "Nissan",
+                                Zone: [{
+                                    ZoneCode: "2c1",
+                                    ZoneName : "Nissan",
+                                }]
+                            },]
+                
+                    },
+                    {
+                        plantCode: "2c4",
+                        plantName : "Nissan",
+                        country:"India",
+                        active: false,
+                            Group: [{
+                                GroupCode: "2c1",
+                                GroupName : "Nissan",
+                                    Zone: [{
+                                    ZoneCode: "2c1",
+                                    ZoneName : "Nissan",
+                                    }]
+                                },{
+                                GroupCode: "2c1",
+                                GroupName : "Nissan",
+                                    Zone: [{
+                                        ZoneCode: "2c1",
+                                        ZoneName : "Nissan",
+                                    }]
+                                },]
+                    
+                        },
+                        {
+                            plantCode: "2c5",
+                            plantName : "Nissan",
+                            country:"India",
+                            active: false,
+                                Group: [{
+                                    GroupCode: "2c1",
+                                    GroupName : "Nissan",
+                                        Zone: [{
+                                        ZoneCode: "2c1",
+                                        ZoneName : "Nissan",
+                                        }]
+                                    },{
+                                    GroupCode: "2c1",
+                                    GroupName : "Nissan",
+                                        Zone: [{
+                                            ZoneCode: "2c1",
+                                            ZoneName : "Nissan",
+                                        }]
+                                    },]
+                        
+                            },
+                            {
+                                plantCode: "2c6",
+                                plantName : "Nissan",
+                                country:"India",
+                                active: false,
+                                    Group: [{
+                                        GroupCode: "2c1",
+                                        GroupName : "Nissan",
+                                            Zone: [{
+                                            ZoneCode: "2c1",
+                                            ZoneName : "Nissan",
+                                            }]
+                                        },{
+                                        GroupCode: "2c1",
+                                        GroupName : "Nissan",
+                                            Zone: [{
+                                                ZoneCode: "2c1",
+                                                ZoneName : "Nissan",
+                                            }]
+                                        },]
+                            
+                                },
             ]
         }
 
-  const [buttonStatus, setButtonStatus] = useState(true);
+ 
   const [open, setOpen] = useState(false);
+  const [stateMockData, setStateMockData]=useState(mockData);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(4);  
+  const [editablePlantCode, setEditablePlantCode]=useState('')
+  const [editablePlantName, setEditablePlantName]=useState('')
 
-  const handleActivateButton =(id)=>{
-    setButtonStatus(false)
-    console.log(id)
-  }
-  const handledeActivateButton =()=>{
-    setButtonStatus(true)
-  }
 
-  const handleOpen = () => setOpen(true);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const handleActivateDeactivateButton =( buttonStatus, item, i)=>{
+      let tempMockData=item
+      let completeMockData=stateMockData;
+      tempMockData.active=buttonStatus ? false : true;
+      completeMockData.push(tempMockData)
+      setStateMockData(completeMockData)  
+  }
+  
+  const handleOpen = (plantCodeValue, plantNameValue) => {
+    setEditablePlantCode(plantCodeValue);
+    setEditablePlantName(plantNameValue);
+    //   console.log("++",editablePlantCodeValue, editablePlantNameValue)
+      setOpen(true); 
+    }
+    
   const handleClose = () => setOpen(false);
        
   return (
     <div>
-        <TableContainer component={Paper}> 
-            <Table sx={{ minWidth: 650 }}   aria-label="a dense table">
+         <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <TableContainer sx={{ maxHeight: 310 }}> 
+            <Table stickyHeader aria-label="sticky table">
                 <TableHead>
                     <TableRow>
                         <TableCell >Plant Code</TableCell>
@@ -97,34 +212,42 @@ function Plant({menuTabValue}) {
                             timeout: 500,
                             }}
                         > 
-                            <CreateUpdate createOrUpdateStatus={'Update'} handleClose={handleClose} headerMenuValue={menuTabValue}/>
+                            <CreateUpdate createOrUpdateStatus={'Update'} handleClose={handleClose} headerMenuValue={menuTabValue} codeValue={editablePlantCode} codeName={editablePlantName}/>
                         </Modal>
                             <TableBody>
-                             {mockData.Plant.map((item) => (
+                             {stateMockData.Plant
+                             .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                             .map((item, i) => (
                                 <TableRow
                                     key="1"
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
-                                    <TableCell >{item.plantCode}</TableCell>
-                                    <TableCell >{item.plantName}</TableCell>
-                                    <TableCell >{item.country}</TableCell>
-                                    <TableCell >
-                                        <Button variant="contained" onClick={handleOpen}>Edit</Button>
+                                    <TableCell width="20%">{item.plantCode}</TableCell>
+                                    <TableCell width="20%">{item.plantName}</TableCell>
+                                    <TableCell width="20%">{item.country}</TableCell>
+                                    <TableCell width="20%">
+                                        <Button variant="contained" onClick={()=>handleOpen(item.plantCode,item.plantName)} startIcon={<EditIcon />}>Edit</Button>
                                     </TableCell>
-                                    <TableCell >
-                                        {buttonStatus ?(<Button variant="contained" onClick={()=>handleActivateButton(item.plantCode)} color="error">
-                                            Deactivate
-                                        </Button>):( <Button variant="contained"  onClick={handledeActivateButton}>
-                                            active
-                                        </Button>) }
-                                       
-                                       
+                                    <TableCell width="20%">
+                                        <Button variant="contained" fullWidth="false" onClick={() => { handleActivateDeactivateButton( item.active, item, i) }}
+                                            color={item.active ? 'success' : 'error'}>{item.active ? 'Activate' : 'Deactivate'}
+                                        </Button>  
                                     </TableCell>
                                 </TableRow>
                               ))}  
                             </TableBody>
                         </Table>
         </TableContainer>
+        <TablePagination
+            rowsPerPageOptions={[4, 10, 25]}
+            component="div"
+            count={stateMockData.Plant.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+       />
+       </Paper>
     </div>
   );
 }
