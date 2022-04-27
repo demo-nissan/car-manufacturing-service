5c2bcd8caac3d489357d6b47c26a46d527e66031
demo-nissan <jojojoseph1096@gmail.com>
2022-04-27 22:35:53 +0530

    Updated API call

--- src/components/Plant/plant.js
+++ src/components/Plant/plant.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import axios from "axios";
+import React from "react";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -11,153 +13,22 @@ import Modal from '@mui/material/Modal';
 import Backdrop from '@mui/material/Backdrop';
 import EditIcon from '@mui/icons-material/Edit';
 import TablePagination from '@mui/material/TablePagination';
-
+import { mockData } from '../mockData/mockData';
 import CreateUpdate from '../createUpdate';
 
-function Plant({menuTabValue}) {
-    let mockData={
-        Plant :  [
-            {
-            plantCode: "2c1",
-            plantName : "Nissan",
-            country:"India",
-            active: true,
-                Group: [{
-                    GroupCode: "2c1",
-                    GroupName : "Nissan",
-                        Zone: [{
-                        ZoneCode: "2c1",
-                        ZoneName : "Nissan",
-                        }]
-                    },{
-                    GroupCode: "2c1",
-                    GroupName : "Nissan",
-                        Zone: [{
-                            ZoneCode: "2c1",
-                            ZoneName : "Nissan",
-                        }]
-                    },]
-        
-            },
-            {
-                plantCode: "2c2",
-                plantName : "Nissan",
-                country:"India",
-                active: false,
-                    Group: [{
-                        GroupCode: "2c1",
-                        GroupName : "Nissan",
-                            Zone: [{
-                            ZoneCode: "2c1",
-                            ZoneName : "Nissan",
-                            }]
-                        },{
-                        GroupCode: "2c1",
-                        GroupName : "Nissan",
-                            Zone: [{
-                                ZoneCode: "2c1",
-                                ZoneName : "Nissan",
-                            }]
-                        },]
-            
-                },
-                {
-                    plantCode: "2c3",
-                    plantName : "Nissan",
-                    country:"India",
-                    active: true,
-                        Group: [{
-                            GroupCode: "2c1",
-                            GroupName : "Nissan",
-                                Zone: [{
-                                ZoneCode: "2c1",
-                                ZoneName : "Nissan",
-                                }]
-                            },{
-                            GroupCode: "2c1",
-                            GroupName : "Nissan",
-                                Zone: [{
-                                    ZoneCode: "2c1",
-                                    ZoneName : "Nissan",
-                                }]
-                            },]
-                
-                    },
-                    {
-                        plantCode: "2c4",
-                        plantName : "Nissan",
-                        country:"India",
-                        active: false,
-                            Group: [{
-                                GroupCode: "2c1",
-                                GroupName : "Nissan",
-                                    Zone: [{
-                                    ZoneCode: "2c1",
-                                    ZoneName : "Nissan",
-                                    }]
-                                },{
-                                GroupCode: "2c1",
-                                GroupName : "Nissan",
-                                    Zone: [{
-                                        ZoneCode: "2c1",
-                                        ZoneName : "Nissan",
-                                    }]
-                                },]
-                    
-                        },
-                        {
-                            plantCode: "2c5",
-                            plantName : "Nissan",
-                            country:"India",
-                            active: false,
-                                Group: [{
-                                    GroupCode: "2c1",
-                                    GroupName : "Nissan",
-                                        Zone: [{
-                                        ZoneCode: "2c1",
-                                        ZoneName : "Nissan",
-                                        }]
-                                    },{
-                                    GroupCode: "2c1",
-                                    GroupName : "Nissan",
-                                        Zone: [{
-                                            ZoneCode: "2c1",
-                                            ZoneName : "Nissan",
-                                        }]
-                                    },]
-                        
-                            },
-                            {
-                                plantCode: "2c6",
-                                plantName : "Nissan",
-                                country:"India",
-                                active: false,
-                                    Group: [{
-                                        GroupCode: "2c1",
-                                        GroupName : "Nissan",
-                                            Zone: [{
-                                            ZoneCode: "2c1",
-                                            ZoneName : "Nissan",
-                                            }]
-                                        },{
-                                        GroupCode: "2c1",
-                                        GroupName : "Nissan",
-                                            Zone: [{
-                                                ZoneCode: "2c1",
-                                                ZoneName : "Nissan",
-                                            }]
-                                        },]
-                            
-                                },
-            ]
-        }
-
- 
+function Plant({menuTabValue}) { 
   const [open, setOpen] = useState(false);
   const [stateMockData, setStateMockData]=useState(mockData.Plant);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(4);  
   const [editableData, setEditableData]=useState('')
+  const [plantIndexValue, setPlantIndexValue]=useState('');
+
+  React.useEffect(() => {
+    axios.get('http://localhost:8080/get/all').then((response) => {
+        setStateMockData(response.data);
+    });
+  }, []);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -173,10 +44,16 @@ function Plant({menuTabValue}) {
     console.log(list);
     list[i]['active'] = buttonStatus ? false : true;
     setStateMockData(list);
+
+    const baseURL = buttonStatus ? 'http://localhost:8080/plants/deactivate/' : 'http://localhost:8080/plants/activate/';
+    axios.put(`${baseURL}/${i}`).then((response) => {
+        console.log(response)
+    });
   }
   
-  const handleOpen = (itemValue) => {
+  const handleOpenEdit = (itemValue, i) => {
     setEditableData(itemValue);
+    setPlantIndexValue(i);
     setOpen(true); 
     }
     
@@ -184,7 +61,7 @@ function Plant({menuTabValue}) {
        
   return (
     <div>
-         <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
         <TableContainer sx={{ maxHeight: 310 }}> 
             <Table stickyHeader aria-label="sticky table">
                 <TableHead>
@@ -207,7 +84,7 @@ function Plant({menuTabValue}) {
                             timeout: 500,
                             }}
                         > 
-                            <CreateUpdate createOrUpdateStatus={'Update'} handleClose={handleClose} headerMenuValue={menuTabValue} updateData={editableData} />
+                            <CreateUpdate createOrUpdateStatus={'Update'} handleClose={handleClose} headerMenuValue={menuTabValue} updateData={editableData} indexValue={plantIndexValue} />
                         </Modal>
                             <TableBody>
                              {stateMockData
@@ -221,7 +98,7 @@ function Plant({menuTabValue}) {
                                     <TableCell width="20%">{item.plantName}</TableCell>
                                     <TableCell width="20%">{item.country}</TableCell>
                                     <TableCell width="20%">
-                                        <Button variant="contained" onClick={()=>handleOpen(item)} startIcon={<EditIcon />}>Edit</Button>
+                                        <Button variant="contained" onClick={()=>handleOpenEdit(item, i)} startIcon={<EditIcon />}>Edit</Button>
                                     </TableCell>
                                     <TableCell width="20%">
                                         <Button variant="contained" fullWidth="false" onClick={() => { handleActivateDeactivateButton( item.active, item, i) }}
